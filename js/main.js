88a23a135d7d9c70260c39197342046763494a83
Your Name <you@example.com>
2022-02-23 21:40:25 +0700

    done

--- js/main.js
+++ js/main.js
@@ -8,3 +8,35 @@ wow = new WOW(
     }
 )
 wow.init();
+
+mobiscroll.datepicker('#check-in', {
+    controls: ['calendar'],
+    calendarType: 'month',
+    showOuterDays: true,
+    pages: 2,
+    selectMultiple: true,
+    responsive: {
+        small: {
+            pages: 1
+        },
+        xsmall: {
+            pages: 1
+        }
+    }
+});
+
+mobiscroll.datepicker('#check-out', {
+    calendarType: 'month',
+    showOuterDays: true,
+    pages: 2,
+    selectMultiple: true,
+    responsive: {
+        small: {
+            pages: 1
+        },
+        xsmall: {
+            pages: 1
+        }
+    }
+});
+
